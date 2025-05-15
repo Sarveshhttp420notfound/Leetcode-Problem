44f8ac3adc78edc1e49de73b859dfe1d1b35b393
Sarvesh kumar singh <87657797+Sarveshhttp420notfound@users.noreply.github.com>
2025-05-15 19:39:50 +0530

    Time: 424 ms (46.75%) | Memory: 0B (100.00%) - LeetSync

---
+++ 3711-first-letter-capitalization-ii/first-letter-capitalization-ii.sql
@@ -0,0 +1,3 @@
+/* Write your PL/SQL query statement below */
+/* Write your PL/SQL query statement below */
+select content_id,content_text as original_text, initcap(content_text) as converted_text from user_content;
