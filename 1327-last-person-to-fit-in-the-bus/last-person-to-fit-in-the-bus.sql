db184d47b741fa354487e8352c1e6f65ee675542
Sarvesh kumar singh <87657797+Sarveshhttp420notfound@users.noreply.github.com>
2025-05-24 21:59:07 +0530

    Time: 783 ms (76.56%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+# Write your MySQL query statement below
+SELECT person_name
+FROM (SELECT person_name, turn,
+    SUM(weight) OVER (ORDER BY turn) AS cum_wei
+    FROM Queue
+) temp 
+WHERE cum_wei <= 1000
+ORDER BY turn DESC LIMIT 1;
